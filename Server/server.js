7f7d2c1be0192761e6a79a97724fa8f258ecf477
Suprateek Sen <suprateeksen62@gmail.com>
2025-08-15 16:22:44 +0530

    Cors policy updated

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,10 @@ import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
