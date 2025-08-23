42332bb10669c428b39a84cf7a3088ef5c908922
SAMEER KUMAR NAYAK <143753418+samerr592004@users.noreply.github.com>
2025-08-23 23:50:41 +0530

    Update Book from.

--- Client/src/store/clientStore.js
+++ Client/src/store/clientStore.js
@@ -3,6 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import { toast } from "react-hot-toast";
 
 export const clientAuthStore = create((set) => ({
+        estimatedPrice: 0,
+        setEstimatedPrice: (price) => set({ estimatedPrice: price }),
       sendQuery: async ({name,email,message}) => {
         try {
             const response = await axiosInstance.post("/client/sendQuery", {
